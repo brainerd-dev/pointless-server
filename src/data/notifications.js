3f22b9f383328d60d358eaaaafa1b1df44d5a8d7
Andrew Brainerd <drwb333@gmail.com>
2020-12-30 14:04:17 -0500

    Add createdByUser to notifications

--- src/data/notifications.js
+++ src/data/notifications.js
@@ -3,11 +3,12 @@ const data = require('../utils/data');
 const log = require('../utils/log');
 const { pusher, pushEvents } = require('../utils/pusher');
 const { NOTIFICATIONS_COLLECTION } = require('../constants/collections');
+const { getUserByEmail } = require('./users');
 
 const getUserNotifications = async (page, size, userEmail) => {
   log.cool(`Getting Notifications for ${userEmail}`);
 
-  const userNotifications = await data.getSome(
+  const notificationData = await data.getSome(
     NOTIFICATIONS_COLLECTION,
     page,
     size,
@@ -17,23 +18,26 @@ const getUserNotifications = async (page, size, userEmail) => {
     { _id: -1 }
   );
 
-  const notifications = {
-    ...userNotifications,
-    items: userNotifications.items.map(notification => {
-      return {
-        ...notification,
-        timestamp: ObjectID(notification._id).getTimestamp()
-      };
-    })
-  };
+  const allNotifications = await Promise.all(notificationData.items.map(async notification => {
+    const user = await getUserByEmail(notification.createdBy);
 
-  return notifications;
+    return {
+      ...notification,
+      timestamp: ObjectID(notification._id).getTimestamp(),
+      createdByUser: user
+    };
+  })).then(notifications => ({
+    ...notificationData,
+    items: notifications
+  }));
+
+  return allNotifications;
 };
 
-const createNotification = async (userEmail, title, message, link) => {
-  log.cool(`Creating Notification`, { userEmail, title, message, link });
+const createNotification = async (createdBy, userEmail, title, message, link) => {
+  log.cool(`Creating Notification`, { createdBy, userEmail, title, message, link });
 
-  const newNotification = await data.insertOne(NOTIFICATIONS_COLLECTION, { userEmail, title, message, link });
+  const newNotification = await data.insertOne(NOTIFICATIONS_COLLECTION, { createdBy, userEmail, title, message, link });
 
   pusher.trigger(userEmail, pushEvents.NOTIFY, newNotification);
 
--- src/data/pools.js
+++ src/data/pools.js
@@ -59,7 +59,9 @@ const addWager = async (poolId, createdBy, wager) => {
       message: userEmail === createdBy ? `Successfully created wager` : `New pool created by ${createdBy}`
     });
 
-    createNotification(userEmail, 'Wager Created', `${createdBy} wants to bet you`);
+    if (userEmail !== createdBy) {
+      createNotification(createdBy, userEmail, 'Wager Created', `${createdBy} wants to bet you`);
+    }
   });
 
   return await data.addToSet(
